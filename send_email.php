85992960a05cb91dbc902eceaadef84ce32d399e
Peter Karanja <peterkaranja0153@gmail.com>
2024-05-28 13:57:27 +0300

    Create send_email.php
    Created a send_mail.php file

---
+++ send_email.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Set email parameters
+    $to = "pmkaranja153@gmail.com"; // Your Gmail address
+    $subject = "New Contact Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    // Create the email body
+    $body = "<h2>Contact Message</h2>";
+    $body .= "<p><strong>Name:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Message:</strong><br>$message</p>";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
